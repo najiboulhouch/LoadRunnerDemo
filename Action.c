de4d7727e92fc5faf30477ec918099f607a791b4
Najib OULHOUCH <developpersgroups@gmail.com>
2022-12-12 15:54:48 +0100

    this is my first commit

---
+++ Action.c
@@ -0,0 +1,13 @@
+Action()
+	{
+	web_custom_request("web_custom_request",
+		"URL=blazedemo.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	return 0;
+}
